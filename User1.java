21a96febc7da8980c6fa49db7cc42d5c81f70144
Sravika-5403 <120772043+Sravika-5403@users.noreply.github.com>
2025-07-23 18:59:03 +0530

    Update User1.java

--- User1.java
+++ User1.java
@@ -1,8 +1,11 @@
 package to_do_list;
-
+import java.util.*;
 public class User1 {
 		public String email;
 		public String name;
 		public int userid;
 		public String pwd;
+		public List<Task> tasks = new ArrayList<>();
+		public List<Task> taskList = new ArrayList<>(); 
+
 	} 
